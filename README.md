38c7ef2f8c16b0b765912ee0ee7bed0ef6e8047b
Jaume Sanchez <the.spite@gmail.com>
2015-01-27 19:14:14 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # THREE.AugmentedConsole.js
-Augmented methods for console.log
+
+THREE.AugmenteConsole.js is a small tool to make development with three.js easier.
+
+It uses chrome's console.table to format the output when using console.log to inspect objects. If the object is a known format it uses a table to display its contents.
+
+The supported types are
+
+* THREE.Vector2, THREE.Vector3, THREE.Vector4
+* THREE.Matrix3, THREE.Matrix4
+* THREE.Euler
+
+#####Demo
+
+Here's a [demo showing how it works](http://www.clicktorelease.com/tools/augmented-console/)
+
+#####Snapshots
+Chrome
+![Chrome](http://www.clicktorelease.com/tools/augmented-console/chrome-augmented-console.jpg "The output on Chrome")
+Firefox
+![Firefox](http://www.clicktorelease.com/tools/augmented-console/firefox-augmented-console.jpg "The output of Firefox")
+
+#####How to use
+Include the script file in your three.js project and the console will be automatically augmented
+
+    <script src="THREE.AugmentedConsole.js"></script>
+
+#####Support
+Chrome and Firefox. Safari does support console.table but doesn't seem to format anything.
+
+Check more stuff here: [clicktorelease.com](http://www.clicktorelease.com) and [twitter](http://twitter.com/thespite).
+
+License
+----
+
+MIT
---
+++ src/THREE.AugmentedConsole.js
@@ -0,0 +1,109 @@
+( function() {
+
+	var _oldConsole = console.log;
+
+	var supportedTypes = [];
+
+	function addSupportedType( type, format ) {
+
+		supportedTypes.push( {
+			type: type,
+			format: format
+		} );
+
+	}
+
+	addSupportedType( THREE.Matrix4, function( v ) {
+
+		var e = v.elements;
+				
+		return [
+			[ e[ 0 ], e[ 1 ], e[ 2 ], e[ 3 ] ],
+			[ e[ 4 ], e[ 5 ], e[ 6 ], e[ 7 ] ],
+			[ e[ 8 ], e[ 9 ], e[ 10 ], e[ 11 ] ],
+			[ e[ 12 ], e[ 13 ], e[ 14 ], e[ 15 ] ]
+		];
+
+	} );
+
+	addSupportedType( THREE.Matrix3, function( v ) {
+
+		var e = v.elements;
+				
+		return [
+			[ e[ 0 ], e[ 1 ], e[ 2 ] ],
+			[ e[ 3 ], e[ 4 ], e[ 5 ] ],
+			[ e[ 6 ], e[ 7 ], e[ 8 ] ]
+		];
+
+	} );
+
+	addSupportedType( THREE.Vector2, function( v ) {
+
+		return {
+			Vector2: { x: v.x, y: v.y }
+		};
+
+	} );
+
+	addSupportedType( THREE.Vector3, function( v ) {
+
+		return {
+			Vector3: { x: v.x, y: v.y, z: v.z }
+		};
+
+	} );
+
+	addSupportedType( THREE.Vector4, function( v ) {
+
+		return {
+			Vector4: { x: v.x, y: v.y, z: v.z, w: v.w }
+		};
+
+	} );
+
+	addSupportedType( THREE.Euler, function( v ) {
+
+		return {
+			Vector4: { x: v.x, y: v.y, z: v.z, order: v.order }
+		};
+
+	} );
+
+	console.log = function() {
+
+		var special = false;
+
+		[].forEach.call( arguments, function( a ) { 
+			supportedTypes.forEach( function( t ) {
+				if( a instanceof t.type ) special = true;;
+			} );
+		} );
+
+		if( special ) {
+
+			[].forEach.call( arguments, function( a ) { 
+			
+				_oldConsole.apply( console, [ a ] );
+
+				supportedTypes.forEach( function( t ) {
+
+					if( a instanceof t.type ) {
+
+						console.table( t.format( a ) );
+
+					}
+					
+				} );
+
+			} );
+
+		} else {
+
+			_oldConsole.apply( console, arguments );
+
+		}
+
+	}
+
+} )();
